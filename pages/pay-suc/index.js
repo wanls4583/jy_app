4cee9c63d1fd827173bf651edf8c239cdda16575
MS-BHETHEPBBLXN\Administrator <409051987@qq.com>
2020-08-13 21:05:54 +0800

    [master]

---
+++ pages/pay-suc/index.js
@@ -0,0 +1,9 @@
+// pages/pay/index.js
+Page({
+    data: {
+
+    },
+    onLoad: function(options) {
+
+    }
+})
--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,9 +1,17 @@
-// pages/pay/index.js
+const app = getApp()
+
 Page({
     data: {
-
+        order: {
+        	title: '商品名称',
+        	orderDate: '2020-09-09 11:11:11',
+        	orderNum: '4423121545645211',
+        	money: 100
+        },
+        payMoney: 100,
+        payWay: 'wechat'
     },
-    onLoad: function(options) {
+    onLoad() {
 
     }
 })
