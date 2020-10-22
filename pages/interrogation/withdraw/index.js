c18b91544c1943453159f4f24964742ce3ea26e6
lisong <409051987@qq.com>
2020-10-22 17:30:05 +0800

    [master]

--- pages/interrogation/withdraw/index.js
+++ pages/interrogation/withdraw/index.js
@@ -23,7 +23,48 @@ Page({
             amount: this.data.doctorInfo.balance
         });
     },
+    //检查金额是否有变化
+    checkAmount() {
+        if (!Number(this.data.amount)) {
+            wx.jyApp.toast('请输入提现金额');
+            return;
+        }
+        if (!this.data.amount > this.data.doctorInfo.balance) {
+            wx.jyApp.toast('提现金额不能大于' + this.data.doctorInfo.balance);
+            return;
+        }
+        return wx.jyApp.http({
+            url: '/systemnotice/settle'
+        }).then((data) => {
+            if (data.notice) {
+                wx.showModal({
+                    title: '提示',
+                    content: data.notice.content,
+                    showCancel: false,
+                    confirmText: '同意',
+                    success(res) {
+                        if (res.confirm) {
+                            this.noticeId = data.notice.id;
+                            this.submit();
+                        }
+                    }
+                });
+            } else {
+                this.submit();
+            }
+        });
+    },
     onSubmit() {
+        this.checkAmount();
+    },
+    //同意
+    read() {
+        wx.jyApp.http({
+            url: `/systemnotice/read/${this.noticeId}`,
+            method: 'post',
+        });
+    },
+    submit() {
         wx.showLoading({
             title: '提交中...',
             mask: true
@@ -44,6 +85,7 @@ Page({
             setTimeout(() => {
                 wx.showToast({ title: '提现成功' });
             }, 500);
+            this.read();
         }).finally(() => {
             wx.hideLoading();
         });
