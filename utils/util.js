e0c79725e1bf4acb112e8e3b44e1b561fffcf099
lisong <409051987@qq.com>
2020-12-28 16:37:25 +0800

    [master]

--- utils/util.js
+++ utils/util.js
@@ -228,7 +228,7 @@ function getAllConfig() {
         'nutritionOrderExpireDay',
         'consultOrderExpireMinute',
         'consultOrderCloseDay',
-        'hideCoategory'
+        'hideCategory'
     ]).then((data) => {
         data.consultOrderCloseHours = data.consultOrderCloseDay * 24 || 0;
         wx.jyApp.store.updateConfigData(data);
