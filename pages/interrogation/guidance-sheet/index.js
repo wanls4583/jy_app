88b8feff13933f527cff693dff016b9136cc9429
lisong <409051987@qq.com>
2021-01-08 17:54:17 +0800

    [master]

--- pages/interrogation/guidance-sheet/index.js
+++ pages/interrogation/guidance-sheet/index.js
@@ -18,6 +18,7 @@ Page({
         }).then((data) => {
             var recipe = data.recipe || {};
             var detail = recipe.detail || {};
+            detail._sex = detail.sex == 1 ? '男' : '女';
             detail.orderTime = detail.orderTime && detail.orderTime.slice(0, 10);
             detail.goods && detail.goods.map((item) => {
                 item._frequency = wx.jyApp.constData.frequencyArray[item.frequency - 1];
@@ -27,7 +28,7 @@ Page({
                     item._unit = wx.jyApp.constData.unitChange[item.unit];
                     item.usage = `${item.days}天，${item._frequency}，每次${item.perUseNum}${wx.jyApp.constData.unitChange[item.standardUnit]}，${item._giveWay}`;
                 } else {
-                    item.usage = `${item.days}天，${item._frequency}，每次1份，${Number(this.data.modulateDose) ? '配制' + this.data.modulateDose + '毫升，' : ''}${item._giveWay}`;
+                    item.usage = `${item.days}天，${item._frequency}，每次1份，${Number(item.modulateDose) ? '配制' + item.modulateDose + '毫升，' : ''}${item._giveWay}`;
                     item._unit = '份';
                 }
             });
