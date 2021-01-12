fc69892fa633f90509d4e78f1a9b56c7d719f432
lisong <409051987@qq.com>
2021-01-12 20:51:39 +0800

    [监管]

---
+++ pages/interrogation/guidance-online/examine-list/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: lisong
+ * @Date: 2020-09-05 22:52:49
+ * @Description: 
+ */
+Page({
+    data: {
+        orderList: [],
+        page: 1,
+        totalPage: -1,
+        stopRefresh: false
+    },
+    onLoad(option) {},
+    onUnload() {},
+    onRefresh() {
+
+    },
+    onLoadMore() {
+
+    },
+    onSave() {}
+})
