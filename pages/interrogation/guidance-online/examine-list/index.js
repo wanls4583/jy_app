929025e3635187e204d5e58171ee90fde684251b
lisong <409051987@qq.com>
2021-01-13 15:02:18 +0800

    [监管]

--- pages/interrogation/guidance-online/examine-list/index.js
+++ pages/interrogation/guidance-online/examine-list/index.js
@@ -10,13 +10,59 @@ Page({
         totalPage: -1,
         stopRefresh: false
     },
-    onLoad(option) {},
+    onLoad(option) {
+        this.loadList();
+    },
     onUnload() {},
     onRefresh() {
-
+        this.loadList(true);
     },
     onLoadMore() {
-
+        this.loadList()
+    },
+    loadList(refresh) {
+        if (refresh) {
+            this.request && this.request.requestTask.abort();
+        } else if (this.loading || this.data.totalPage > -1 && this.data.page > this.data.totalPage) {
+            return;
+        }
+        this.loading = true;
+        this.request = wx.jyApp.http({
+            url: '/nutritionorder/approve/list',
+            data: {
+                page: refresh ? 1 : this.data.page,
+                limit: 20,
+            }
+        });
+        this.request.then((data) => {
+            if (refresh) {
+                this.setData({
+                    page: 1,
+                    totalPage: -1,
+                    list: []
+                });
+            }
+            data.page.list = data.page.list || [];
+            data.page.list.map((item) => {
+                item.patient._sex = item.patient.sex == 1 ? '男' : '女';
+                item.patient.BMI = (item.patient.weight) / (item.patient.height * item.patient.height / 10000);
+                item.patient.BMI = item.patient.BMI && item.patient.BMI.toFixed(2) || '';
+            });
+            this.data.orderList = this.data.orderList.concat(data.page.list);
+            this.setData({
+                list: this.data.orderList,
+                page: this.data.page + 1,
+                totalPage: data.page.totalPage,
+            });
+        }).catch((err) => {
+            console.log(err);
+        }).finally(() => {
+            this.loading = false;
+            this.request = null;
+            this.setData({
+                stopRefresh: true
+            });
+        });
     },
     onSave() {}
 })
