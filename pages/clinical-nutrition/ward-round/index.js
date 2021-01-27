6aa183e53012c2cb40a2325a46527d95ac326773
lisong <409051987@qq.com>
2021-01-27 18:34:52 +0800

    [移动端]

---
+++ pages/clinical-nutrition/ward-round/index.js
@@ -0,0 +1,35 @@
+/*
+ * @Author: lisong
+ * @Date: 2021-01-27 18:14:26
+ * @Description: 
+ */
+Component({
+    options: {
+        styleIsolation: 'apply-shared'
+    },
+    properties: {
+        patient: {
+            type: Object,
+            value: {}
+        },
+        option: {
+            type: Object,
+            value: {}
+        },
+    },
+    data: {
+    },
+    lifetimes: {
+        attached() {
+            this._attached();
+        }
+    },
+    attached: function () {
+        this._attached();
+    },
+    methods: {
+        _attached() {
+
+        },
+    }
+})
