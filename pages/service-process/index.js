d455200e3c55d2a1671b6fc25b2e5f3fa9553590
lisong <409051987@qq.com>
2021-09-08 16:40:40 +0800

    [master]

---
+++ pages/service-process/index.js
@@ -0,0 +1,41 @@
+/*
+ * @Author: lisong
+ * @Date: 2020-09-05 22:52:49
+ * @Description: 
+ */
+Page({
+    data: {
+        active: 0
+    },
+    onLoad(option) {
+        this.storeBindings = wx.jyApp.createStoreBindings(this, {
+            store: wx.jyApp.store,
+            fields: ['configData'],
+        });
+        this.storeBindings.updateStoreBindings();
+        this.setData({
+            from: 'home'
+        });
+    },
+    onUnload() {
+        this.storeBindings.destroyStoreBindings();
+    },
+    onGoto(e) {
+        wx.jyApp.utils.navigateTo(e);
+    },
+    onChangeSwiper(e) {
+        this.setData({
+            active: e.detail.current
+        });
+    },
+    onChangeTab(e) {
+        this.setData({
+            active: e.detail.index
+        });
+    },
+    onClickHome() {
+        wx.reLaunch({
+            url: '/pages/index/index'
+        });
+    }
+})
