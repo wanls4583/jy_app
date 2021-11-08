0cc1d2e1cb3decb9288c13ee9f454a8b840072ba
lisong <409051987@qq.com>
2021-11-08 11:20:09 +0800

    [master]

--- pages/interrogation/chat/index.js
+++ pages/interrogation/chat/index.js
@@ -269,21 +269,24 @@ Page({
     },
     //转诊
     onReferral() {
-        wx.jyApp.dialog.confirm({
-            message: '确定邀请营养师会诊？'
-        }).then(() => {
-            wx.jyApp.http({
-                url: '/consultorder/transfer',
-                method: 'post',
-                data: {
-                    id: this.data.consultOrderId
-                }
-            }).then(() => {
-                this.setData({
-                    'consultOrder.status': 8,
-                    inputHeight: 0
-                });
-            });
+        // wx.jyApp.dialog.confirm({
+        //     message: '确定邀请营养师会诊？'
+        // }).then(() => {
+        //     wx.jyApp.http({
+        //         url: '/consultorder/transfer',
+        //         method: 'post',
+        //         data: {
+        //             id: this.data.consultOrderId
+        //         }
+        //     }).then(() => {
+        //         this.setData({
+        //             'consultOrder.status': 8,
+        //             inputHeight: 0
+        //         });
+        //     });
+        // });
+        wx.jyApp.navigateTo({
+            url: '/pages/interrogation/transfer-list/index?consultOrderId=' + this.data.consultOrderId
         });
     },
     //发文字消息
@@ -792,7 +795,7 @@ Page({
                         _item.nutritionOrderChatVO = _item.nutritionOrderChatVO || {};
                         _item.nutritionOrderChatVO._status = wx.jyApp.constData.mallOrderStatusMap[obj.status];
                     }
-                    if ([7, 8, 9, 10, 11,12,13,14,15,16,17,18,19,20,21,22,23,24,25].indexOf(obj.type) > -1) {
+                    if ([7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25].indexOf(obj.type) > -1) {
                         _item.filtrateChatVO = _item.orderApplyVO || {};
                         _item.filtrateChatVO.associateId = obj.associateId;
                         _item.filtrateChatVO.filtrateResult = obj.filtrateResult;
---
+++ pages/interrogation/transfer-list/index.js
@@ -0,0 +1,136 @@
+Page({
+    data: {
+        searchText: '',
+        searchTipVisible: false,
+        stopRefresh: false,
+        doctorList: [],
+        recentList: [],
+        page: 1,
+        limit: 20,
+        totalPage: -1,
+        firstLoad: false
+    },
+    onLoad(option) {
+        this.storeBindings = wx.jyApp.createStoreBindings(this, {
+            store: wx.jyApp.store,
+            fields: ['configData'],
+        });
+        this.storeBindings.updateStoreBindings();
+        this.loadRecent();
+        this.doctorIds = [];
+        this.consultOrderId = option.consultOrderId;
+    },
+    onUnload() {
+        this.storeBindings.destroyStoreBindings();
+    },
+    onShow() {
+
+    },
+    onChangeText(e) {
+        this.setData({
+            searchText: e.detail
+        });
+    },
+    onCancel() {
+        this.setData({
+            searchTipVisible: false
+        });
+    },
+    onFocus() {
+        this.setData({
+            searchTipVisible: true
+        });
+    },
+    onSearch() {
+        this.setData({
+            searchTipVisible: false,
+            firstLoad: false
+        });
+        this.complexName = this.data.searchText;
+        this.loadList(true);
+    },
+    onRefresh() {
+        this.loadList(true);
+    },
+    onLoadMore() {
+        this.loadList();
+    },
+    onGoto(e) {
+        // wx.jyApp.utils.navigateTo(e);
+    },
+    onCheckboxChange(e) {
+        this.doctorIds = e.detail.values;
+    },
+    onSubmit() {
+        wx.showLoading('转诊中...', true);
+        wx.jyApp.dialog.confirm({
+            message: '确定转诊改问诊单？'
+        }).then(() => {
+            wx.jyApp.http({
+                url: '/consultorder/transfer',
+                method: 'post',
+                data: {
+                    id: this.consultOrderId,
+                    doctorIds: this.doctorIds.join(',')
+                }
+            }).then(() => {
+                wx.jyApp.toastBack('转诊成功');
+            }).finally(() => {
+                wx.hideLoading();
+            });
+        });
+    },
+    //加载医生列表
+    loadList(refresh) {
+        if (refresh) {
+            this.request && this.request.requestTask.abort();
+        } else if (this.loading || this.data.totalPage > -1 && this.data.page > this.data.totalPage) {
+            return;
+        }
+        this.loading = true;
+        this.request = wx.jyApp.http({
+            url: '/doctor/list',
+            data: {
+                page: refresh ? 1 : this.data.page,
+                limit: this.data.limit,
+                complexName: this.complexName || '',
+            }
+        });
+        this.request.then((data) => {
+            if (refresh) {
+                this.setData({
+                    doctorList: [],
+                    page: 1,
+                    limit: 10,
+                    totalPage: -1,
+                    stopRefresh: false,
+                });
+            }
+            this.setData({
+                'page': this.data.page + 1,
+                'totalPage': data.page.totalPage,
+                'doctorList': this.data.doctorList.concat(data.page.list || [])
+            });
+        }).finally(() => {
+            this.loading = false;
+            this.request = null;
+            this.setData({
+                stopRefresh: true
+            });
+        });
+        return this.request;
+    },
+    loadRecent() {
+        wx.jyApp.http({
+            url: '/doctor/recent/transfer/list',
+        }).then((data) => {
+            this.setData({
+                recentList: data.list || [],
+                firstLoad: data.list.length > 0
+            });
+            if (!this.data.firstLoad) {
+                this.loadList();
+            }
+        });
+    }
+})
